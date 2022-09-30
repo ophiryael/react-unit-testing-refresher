d496be047c7e633d14c7490b2c27cf6bc8ba88a9
Ophir Yael <ofir.yael@gmail.com>
2022-09-30 14:20:02 +0300

    Wrap test in test suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    render(<Greeting />);
 
-  const helloWorldElement = screen.getByText("Hello World!");
-  expect(helloWorldElement).toBeInTheDocument();
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
